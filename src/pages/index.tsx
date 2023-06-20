f4343fb29b322b053c4ce65f6b00cc27e57aa982
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-20 11:55:58 -0300

    feat: implementação da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,153 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect, useState } from 'react';
+import Card from '@/components/Card';
+import Form from '@/components/Form';
+import Head from 'next/head';
+import { validateForm } from '../utils/validateForm';
 
 export default function Home() {
+  const [card, setCard] = useState({
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
+    cardImage: '',
+    cardRare: 'normal',
+    cardTrunfo: false
+  });
+
+  const [isSaveButtonDisabled, setButton] = useState(true);
+  const [hasTrunfo, setHasTrunfo] = useState(Boolean);
+  const [cardsList, setCardsList] = useState([]);
+  const [showCards, setShowCards] = useState(false);
+
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
+    cardTrunfo
+  } = card;
+
+  useEffect(() => {
+    const recovery = localStorage.getItem('cardsList') || [];
+    if (recovery.length > 0) setCardsList(JSON.parse(recovery as string));
+  }, []);
+
+  useEffect(() => validateForm(card) ? setButton(false) : setButton(true), [card]);
+
+  function onInputChange(target: any) {
+    const { name } = target;
+    const value = target.type !== 'checkbox' ? target.value : target.checked;
+    setCard({ ...card, [name]: value });
+
+    return;
+  }
+
+  function onSaveButtonClick() {
+    if (cardTrunfo) setHasTrunfo(true);
+
+    const newListCards = cardsList.concat({ ...card } as any);
+    setCardsList(newListCards);
+    localStorage.setItem('cardsList', JSON.stringify(newListCards));
+
+    setCard({
+      cardTrunfo: false,
+      cardName: '',
+      cardDescription: '',
+      cardImage: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardRare: 'normal'
+    });
+
+    return;
+  }
+
+  function deleteCards(cardIndex: number) {
+    const filteredCardList = cardsList.filter((_, i) => cardIndex !== i);
+    if (!filteredCardList.some(({ cardTrunfo }) => cardTrunfo === true)) setHasTrunfo(false);
+    setCardsList(filteredCardList);
+    localStorage.setItem('cardsList', JSON.stringify(filteredCardList));
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <title>Trunfo</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Form
+        cardName={cardName}
+        cardDescription={cardDescription}
+        cardAttr1={cardAttr1}
+        cardAttr2={cardAttr2}
+        cardAttr3={cardAttr3}
+        cardImage={cardImage}
+        cardRare={cardRare}
+        cardTrunfo={cardTrunfo}
+        hasTrunfo={hasTrunfo}
+        isSaveButtonDisabled={isSaveButtonDisabled}
+        onInputChange={onInputChange}
+        onSaveButtonClick={onSaveButtonClick}
+      />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h2>Pré visualização</h2>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Card
+        cardName={cardName}
+        cardDescription={cardDescription}
+        cardAttr1={cardAttr1}
+        cardAttr2={cardAttr2}
+        cardAttr3={cardAttr3}
+        cardImage={cardImage}
+        cardRare={cardRare}
+        cardTrunfo={cardTrunfo}
+      />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <button
+        type="button"
+        onClick={() => setShowCards(!showCards)}
+        className="border border-black"
+      >
+        Mostrar Cartas
+      </button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      {showCards && (
+        <ul>
+          {cardsList.length > 0 && (
+            cardsList.map((c: any, index: number) => (
+              <li
+                key={`${c.cardName}-${index}`}
+                className="border border-black m-1 p-1"
+              >
+                <Card
+                  cardName={c.cardName}
+                  cardDescription={c.cardDescription}
+                  cardAttr1={c.cardAttr1}
+                  cardAttr2={c.cardAttr2}
+                  cardAttr3={c.cardAttr3}
+                  cardImage={c.cardImage}
+                  cardRare={c.cardRare}
+                  cardTrunfo={c.cardTrunfo}
+                />
+                <button
+                  type="button"
+                  className="border border-black"
+                  onClick={() => deleteCards(index)}
+                >
+                  Excluir
+                </button>
+              </li>)
+            )
+          )}
+        </ul>
+      )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
